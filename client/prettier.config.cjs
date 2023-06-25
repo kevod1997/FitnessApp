3ec60ee4a12a17dbcf69850759899da409509540
Kevin Defalco <kevindefalco@gmail.com>
2023-06-24 22:08:32 -0300

    first commit

---
+++ client/prettier.config.cjs
@@ -0,0 +1,5 @@
+export default {
+    plugins: [require("prettier-plugin-tailwindcss")]
+  }
+  
+  
---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <div className="bg-gray-20">app</div>
+  )
+}
+
+export default App
