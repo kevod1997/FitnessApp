c6767e08b2f47d4b6c698452cf2883142777e07f
Kevin Defalco <kevindefalco@gmail.com>
2023-07-10 23:22:09 -0300

    creamos el navbar

---
+++ client/src/scenes/home/index.tsx
@@ -0,0 +1,25 @@
+import useMediaQuery from '@/hooks/useMediaQuery'
+import { SelectedPage } from '@/shared/types'
+import ActionButton from '@/shared/ActionButton'
+import HomePageText from "@/assets/HomePageText.png"
+import HomePageGraphic from "@/assets/HomePageGraphic.png"
+import SponsorRedBull from "@/assets/SponsorRedBull.png"
+import SponsorForbes from "@/assets/SponsorForbes.png"
+import SponsorFortune from "@/assets/SponsorFortune.png"
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const Home = ({setSelectedPage}: Props) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  return (
+    <section
+    id="home "
+    >
+        
+    </section>
+  )
+}
+
+export default Home
