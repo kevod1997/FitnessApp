41c2d1c6e1c6ad757de13b4cdefadb0b08ee3c26
Kevin Defalco <kevindefalco@gmail.com>
2023-07-11 23:46:07 -0300

    home design

--- client/src/scenes/home/index.tsx
+++ client/src/scenes/home/index.tsx
@@ -6,6 +6,7 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png"
 import SponsorRedBull from "@/assets/SponsorRedBull.png"
 import SponsorForbes from "@/assets/SponsorForbes.png"
 import SponsorFortune from "@/assets/SponsorFortune.png"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -16,8 +17,37 @@ const Home = ({setSelectedPage}: Props) => {
   return (
     <section
     id="home "
+    className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
     >
-        
+        {/* Image and main header */}
+        <div>
+          {/* Main header */}
+          <div>
+        {/* headings */}
+        <div>
+          <div>
+            <div>
+              <img alt='home-page-text' src={HomePageText}/>
+            </div>
+          </div>
+          <p>
+            LOCURA TOTAL
+          </p>
+        </div>
+            {/* ACTIONS */}
+            <div>
+              <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+              <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+              onClick={()=> setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}>
+
+              </AnchorLink>
+            </div>
+          </div>
+
+          {/* image */}
+          <div></div>
+        </div>
     </section>
   )
 }
